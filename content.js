53ccf7b2e09396c7dccbaa08a4fc5648e2ea8532
Om Patel <pate3537@shernet.sheridancollege.ca>
2023-06-01 18:21:29 -0400

    Manifest version 3

---
+++ content.js
@@ -0,0 +1,15 @@
+// function createSidebar() {
+//     const sidebar = document.createElement("iframe");
+//     sidebar.src = chrome.runtime.getURL("popup.html");
+//     sidebar.style.position = "fixed";
+//     sidebar.style.top = "0";
+//     sidebar.style.right = "0";
+//     sidebar.style.width = "300px";
+//     sidebar.style.height = "100%";
+//     sidebar.style.border = "none";
+//     sidebar.style.zIndex = "999999";
+//     document.body.appendChild(sidebar);
+//   }
+  
+//   createSidebar();
+  
